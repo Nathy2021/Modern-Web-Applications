171678f4af95475930da4a0ecf7ff0e3cb2f5143
NATHNAEL GEBREGZIABHER WELDEGIORGIS <86237147+Nathy2021@users.noreply.github.com>
2021-10-13 00:15:28 -0500

    Week3 Day2

--- Assignments/week3Day2/app01/app01.js
+++ Assignments/week3Day2/app01/app01.js
@@ -1,41 +1,124 @@
-// var http = require("http")
-// const server = http.createServer(function(req, res){
-//     req.write(req.random);
-//     res.end();
-// });
-// server.on('request', (req, res) => {
-//   res.writeHead(...);
-//   res.end(...server response);
-// });
-// server.listen(port, someURL);
+let http = require('http');
+let fs = require('fs');
+var url = require('url');  
 
-// var http = require('http');
-// var url = require('url');
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   var q = url.parse(req.url, true).query;
-//   var txt = q.year + " " + q.month;
-//   res.end(txt);
-// }).listen(8080);
-
-// var http = require('http');
-// http.createServer(function(req, res){
-//     console.log("req.method: ", req.method, req.url);
-//     // res. 
-//     // req.random();
-//     res.end();   
-
-// }).listen(4444);
-
-var http = require('http');
-const { type } = require('os');
-var url = require('url');
-var fs = require('fs');
 http.createServer(function(req, res){
-    console.log("req.method: ", req.method, req.url);
-    res.writeHead(200, {"content-type":"text/html"});
-    res.send("./public/page1.html");
-    res.end("hello world\n");   
+    var path = url.parse(req.url).pathname; 
+    res.writeHead(200, {'Content-Type': 'text/html'});  
+   
+    console.log(path); 
+  
+    switch(path)
+    {
+     
+        case '/index.html': 
+            var q = url.parse(req.url, true).query;
+            console.log(q);
+            var pageNumber = q.page;
+            console.log("page number ",pageNumber)
 
+            if(pageNumber == 1){
+                fs.readFile('./public/page1.html', null, function (error, data) {
+                    if (error) {
+                        console.log("5");
+                        res.writeHead(404);
+                        res.write(' File not found!');
+                    } else {
+                        res.write(data);
+                    }
+                    res.end();
+                });              
+
+            }           
+            
+            else if(pageNumber ==2){
+                fs.readFile('./public/page2.html', null, function (error, data) {
+                    if (error) {
+                     console.log("7");
+                        res.writeHead(404);
+                        res.write('Whoops! File not found!');
+                    } else {
+                        res.write(data);
+                    }
+                    res.end();
+                });
+
+            }  
+            
+            else if(pageNumber ==3){
+                fs.readFile('./public/page3.html', null, function (error, data) {
+                    if (error) {
+                     console.log("7");
+                        res.writeHead(404);
+                        res.write('Whoops! File not found!');
+                    } else {
+                        res.write(data);
+                    }
+                    res.end();
+                });
+
+            }
+            
+        
+            else {
+                fs.readFile('/public/index', null, function (error, data) {
+                    if (error) {
+                        console.log("4");
+                        res.writeHead(404);
+                        res.write('File not found!');
+                    } else {
+                        res.write(data);
+                    }
+                    res.end();
+                });
+
+            }           
+           break;
+
+        case '/page1.html': 
+             fs.readFile('./public/page1.html', null, function (error, data) {
+                    if (error) {
+                        console.log("5");
+                        res.writeHead(404);
+                        res.write(' File not found!');
+                    } else {
+                        res.write(data);
+                    }
+                    res.end();
+                });
+                break;
+            case '/page2.html': 
+                fs.readFile('./public/page2.html', null, function (error, data) {
+                       if (error) {
+                        console.log("6");
+                           res.writeHead(404);
+                           res.write('File not found!');
+                       } else {
+                           res.write(data);
+                       }
+                       res.end();
+                   });
+                   break;
+
+                case '/page3.html': 
+                     fs.readFile('./public/page3.html', null, function (error, data) {
+                       if (error) {
+                        console.log("7");
+                           res.writeHead(404);
+                           res.write('Whoops! File not found!');
+                       } else {
+                           res.write(data);
+                       }
+                       res.end();
+                   });
+                   break;
+                  
+                 default:  
+                       res.writeHead(404);  
+                       res.write("opps check your url - 404");  
+                       res.end();  
+                       break; 
+    }
 }).listen(4444);
+
+
